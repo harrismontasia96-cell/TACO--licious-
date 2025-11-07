42e12a1c4f590191a185ac9deaa395a97b78e798
montasia harris <mharris@my.yearupunited.com>
2025-11-07 13:43:05 -0500

    Tacolicious Capstone2

--- src/main/java/com/pluralsight/Taco.java
+++ src/main/java/com/pluralsight/Taco.java
@@ -2,43 +2,46 @@ package com.pluralsight;
 
 import java.util.List;
 
+                  //Using implements so this class can agree with and fallowing
+                  //structure defined by my Interface
 public class Taco implements MenuItem{
-    public enum Size { SINGLE, THREE_TACO_PLATE, BURRITO }
-    public enum Shell { CORN, FLOUR, HARD_SHELL, BOWL }
+    public enum Size { SINGLE, THREE_TACO_PLATE, BURRITO } //Ordinals/Value) - 0,1,2
+    public enum Shell { CORN, FLOUR, HARD_SHELL, BOWL }//Ordinals/(Value) - 0,1,2,3
 
-    private Size size;
-    private Shell shell;
+    private Size size;//My parameters (Variables)/(Objects)
+    private Shell shell;     //input for my methods or constructors to call on
     private List<Topping> toppings;
     private boolean deepFried;
 
+                                   //Constructor method to construct my (Objects)
     public Taco(Size size, Shell shell, List<Topping> toppings, boolean deepFried) {
-        this.size = size;
-        this.shell = shell;
+        this.size = size;//keyword This to refer to the current (Object)
+        this.shell = shell;             //Passing the Parameter the Constructor
         this.toppings = toppings;
         this.deepFried = deepFried;
     }
 
-    @Override
+    @Override// an METHOD that has override-equivalent to that of any public method declared in Object.
     public double getPrice() {
         double price = 0;
-        switch(size) {
-            case SINGLE: price += 3.0; break;
-            case THREE_TACO_PLATE: price += 8.0; break;
-            case BURRITO: price += 7.0; break;
+        switch(size) {//our switch statement to test against multiple possible values
+            case SINGLE: price += 3.0; break; // Case - a possible match for (Value) being tested
+            case THREE_TACO_PLATE: price += 8.0; break;  //Break stops the switch statement
+            case BURRITO: price += 7.0; break;           //once a match is found and to prevent Fall-Through
         }
-        for(Topping t : toppings) {
-            if(t.isPremium()) price += t.getPrice();
-        }
-        if(deepFried) price += 1.0; // extra for deep fried
-        return price;
+        for(Topping t : toppings) {         // if statement made inside a for loop
+            if(t.isPremium()) price += t.getPrice();// checking if the condition is
+        }                                          //premium if :yes Adding additional pricing
+        if(deepFried) price += 1.0; //checking condition if user input is
+        return price;                 //wanting DeepFried it is extra for DeepFried
     }
 
-    @Override
-    public String getDescription() {
+    @Override// an METHOD that has override-equivalent to that of any public method declared in Object.
+    public String getDescription() {//using StringBuilder so im able to append,insert,delete,modify my string without having to make a new one every time
         StringBuilder desc = new StringBuilder(size + " taco with " + shell + " shell, toppings: ");
         for(Topping t : toppings) {
-            desc.append(t.getName()).append(", ");
-        }
+            desc.append(t.getName()).append(", ");// using append to add or append (Variables)
+        }                                         // at the end of an existing StringBuilder
         if(deepFried) desc.append("deep fried");
         return desc.toString();
     }
--- src/main/java/com/pluralsight/Topping.java
+++ src/main/java/com/pluralsight/Topping.java
@@ -1,17 +1,18 @@
 package com.pluralsight;
 
 public class Topping {
-    private String name;
-    private boolean premium;
+    private String name;//My parameters (Variables)/(Objects
+    private boolean premium;//input for my methods or constructors to call on
     private double price;
 
+    //Constructor method to construct my (Objects)
     public Topping(String name, boolean premium, double price) {
-        this.name = name;
-        this.premium = premium;
+        this.name = name;//keyword This to refer to the current (Object)
+        this.premium = premium;    //Passing the Parameter the Constructor
         this.price = price;
     }
 
-    public String getName() { return name; }
+    public String getName() { return name; } //using get to retrieve a value
     public boolean isPremium() { return premium; }
     public double getPrice() { return price; }
 }
